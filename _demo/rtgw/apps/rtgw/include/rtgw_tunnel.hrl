9642eaea654a140680781a6a3460ee24eca77c7a
Denis Fakhrtdinov <me@shizz.ru>
2023-07-24 18:31:47 +0300

    Cleanup the demo applications

--- _demo/rtgw/apps/rtgw/include/rtgw_tunnel.hrl
+++
@@ -1,8 +0,0 @@
--record(tunnel, {
-  name :: binary(),
-  secret :: binary(),
-  host :: binary(),
-  port :: pos_integer(),
-  traffic_in_bytes = 0 :: non_neg_integer(),
-  traffic_out_bytes = 0 :: non_neg_integer()
-}).
--- _demo/rtgw/apps/rtgw_grpc/include/rtgw_grpc_client.hrl
+++
@@ -1,8 +0,0 @@
-%% gRPC events messages
-
--define(grpc_event_stream_killed(StreamRef), {router_grpc_client, stream_killed, StreamRef}).
--define(grpc_event_stream_unprocessed(StreamRef), {router_grpc_client, stream_unprocessed, StreamRef}).
--define(grpc_event_connection_down(StreamRef), {router_grpc_client, connection_down, StreamRef}).
--define(grpc_event_response(StreamRef, IsFin, Status, Headers), {router_grpc_client, response, StreamRef, IsFin, Status, Headers}).
--define(grpc_event_data(StreamRef, IsFin, Data), {router_grpc_client, data, StreamRef, IsFin, Data}).
--define(grpc_event_trailers(StreamRef, Trailers), {router_grpc_client, trailers, StreamRef, Trailers}).
